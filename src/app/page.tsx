8d34cba34e1c6cf632335e9c03d3a839c3e2df3e
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2023-10-16 21:50:36 -0300

    feat: header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-white'>Home</h1>
+
     </main>
   )
 }
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,26 @@
+import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { Button } from "./button";
+import { Card } from "./card";
+
+const Header = () => {
+    return (
+        <Card className="flex items-center justify-between p-[1.875rem]">
+
+            <Button size="icon" variant="outline">
+                <MenuIcon />
+            </Button>
+
+            <h1 className="text-lg font-semibold">
+                <span className="text-primary">NIC</span>
+                STORE
+            </h1>
+
+            <Button size="icon" variant="outline">
+                <ShoppingCartIcon />
+            </Button>
+
+        </Card>
+    )
+}
+
+export default Header;
