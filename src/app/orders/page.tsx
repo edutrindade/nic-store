ee56fa296a715495003e9ea3b08dc1c8333b9b7c
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2023-10-24 00:03:34 -0300

    Finalização do layout e integração com stripe

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,49 @@
+import { Badge } from "@/components/ui/badge";
+import { authOptions } from "@/lib/auth";
+import { prismaClient } from "@/lib/prisma";
+import { PackageSearchIcon } from "lucide-react";
+import { getServerSession } from "next-auth";
+import OrderItem from "./components/order-item";
+
+export const dynamic = "force-dynamic";
+
+async function OrderPage() {
+    const user = getServerSession(authOptions);
+
+    if (!user) {
+        return <p>Acesso não permitido! Autentique-se primeiro 😊</p>;
+    }
+
+    const orders = await prismaClient.order.findMany({
+        where: {
+            userId: (user as any).id,
+        },
+        include: {
+            orderProducts: {
+                include: {
+                    product: true,
+                },
+            },
+        },
+    });
+
+    return (
+        <div className="p-5">
+            <Badge
+                className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+                variant="outline"
+            >
+                <PackageSearchIcon size={16} />
+                Meus Pedidos
+            </Badge>
+
+            <div className="mt-5 flex flex-col gap-5">
+                {orders.map((order) => (
+                    <OrderItem key={order.id} order={order} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default OrderPage;
--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,6 +2,8 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
+import DiscountBadge from "./discount-badge";
 
 interface ProductItemProps {
     product: ProductWithTotalPrice;
@@ -9,50 +11,52 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
     return (
-        <div className="flex flex-col gap-4">
-            <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
-                <Image
-                    src={product.imageUrls[0]}
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className="w-auto h-auto max-w-[80%] max-h-[70%]"
-                    style={{
-                        objectFit: "contain",
-                    }}
-                    alt={product.name}
-                />
+        <Link href={`/product/${product.slug}`}>
+            <div className="flex flex-col gap-4">
+                <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+                    <Image
+                        src={product.imageUrls[0]}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="w-auto h-auto max-w-[80%] max-h-[70%]"
+                        style={{
+                            objectFit: "contain",
+                        }}
+                        alt={product.name}
+                    />
 
-                {product.discountPercentage > 0 && (
-                    <Badge className="absolute left-2 top-3 px-2 py-[2px]">
-                        <ArrowDownIcon size={14} /> {product.discountPercentage}%
-                    </Badge>
-                )}
-            </div>
-
-            <div className="flex flex-col gap-1">
-                <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
-                    {product.name}
-                </p>
-
-                <div className="flex items-center gap-2">
-                    {product.discountPercentage > 0 ? (
-                        <>
-                            <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
-                                R$ {product.totalPrice?.toFixed(2).replace('.', ',')}
-                            </p>
-                            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
-                                R$ {Number(product.basePrice).toFixed(2).replace('.', ',')}
-                            </p>
-                        </>
-                    ) : (
-                        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
-                            R$ {product.basePrice.toFixed(2).replace('.', ',')}
-                        </p>
+                    {product.discountPercentage > 0 && (
+                        <DiscountBadge className="absolute left-3 top-3">
+                            {product.discountPercentage}
+                        </DiscountBadge>
                     )}
                 </div>
+
+                <div className="flex flex-col gap-1">
+                    <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
+                        {product.name}
+                    </p>
+
+                    <div className="flex items-center gap-2">
+                        {product.discountPercentage > 0 ? (
+                            <>
+                                <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
+                                    R$ {product.totalPrice?.toFixed(2).replace('.', ',')}
+                                </p>
+                                <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
+                                    R$ {Number(product.basePrice).toFixed(2).replace('.', ',')}
+                                </p>
+                            </>
+                        ) : (
+                            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
+                                R$ {product.basePrice.toFixed(2).replace('.', ',')}
+                            </p>
+                        )}
+                    </div>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
